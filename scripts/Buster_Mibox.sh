02ac5147f2d21735744b4d8bea3c604611119a8f
speja969 <62497469+speja969@users.noreply.github.com>
2020-10-09 09:42:59 +0200

    Update Buster_Mibox.sh

--- scripts/Buster_Mibox.sh
+++ scripts/Buster_Mibox.sh
@@ -19,7 +19,7 @@ sudo apt-get install -y software-properties-common && sudo apt-add-repository co
 
 sudo apt-get update
 
-sudo apt-get install -y openbox obmenu firefox-esr tint2 rxvt-unicode menu xsel pulseaudio pavucontrol mlocate lxappearance arandr apt-file synaptic firmware-linux firmware-linux-nonfree xutils mesa-utils xarchiver htop wicd gnome-disk-utility python3-pip python-pip ttf-mscorefonts-installer fonts-ubuntu fonts-ubuntu-console suckless-tools simplescreenrecorder geany gdebi fbxkb mpv curl gmrun xscreensaver  pnmixer sxiv scrot xsettingsd ffmpeg git qbittorrent wmctrl xinit xorg xserver-xorg
+sudo apt-get install -y openbox obmenu firefox-esr tint2 rxvt-unicode menu xsel pulseaudio pavucontrol mlocate lxappearance arandr apt-file synaptic firmware-linux firmware-linux-nonfree xutils mesa-utils xarchiver htop wicd gnome-disk-utility python3-pip python-pip ttf-mscorefonts-installer fonts-ubuntu fonts-ubuntu-console suckless-tools simplescreenrecorder geany gdebi fbxkb mpv curl gmrun xscreensaver  pnmixer sxiv scrot xsettingsd ffmpeg git apt-rdepends transmission-common wmctrl xinit xorg xserver-xorg
 
 sudo apt install pcmanfm-qt --no-install-recommends
 sudo apt autoremove
